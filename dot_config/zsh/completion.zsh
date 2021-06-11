4cf212c62de8b0d159d650e616212e54588f9565
Spy Kab <ooo000spy000ooo@gmail.com>
2021-06-11 12:49:58 +0700

    Update completion

--- dot_config/zsh/completion.zsh
+++ dot_config/zsh/completion.zsh
@@ -19,7 +19,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 # pip
-eval $(pip3 completion --zsh)
+if [ $commands[helm] ]; then eval $(pip3 completion --zsh); fi
 # pyenv
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
