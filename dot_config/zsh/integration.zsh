2aa215b3383cdbe93e3a79f2d6d978a3c40ae19b
Spy Kab <ooo000spy000ooo@gmail.com>
2021-06-11 12:46:27 +0700

    Update zshrc

---
+++ dot_config/zsh/integration.zsh
@@ -0,0 +1,35 @@
+# Perl
+# eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"
+
+# kube-ps1
+[ -f /usr/local/opt/kube-ps1/share/kube-ps1.sh ] && source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
+PS1='$(kube_ps1)'$PS1
+
+# Add iterm2 shell integration
+# https://iterm2.com/documentation-shell-integration.html
+[ -f ~/.iterm2_shell_integration.zsh ] && source ~/.iterm2_shell_integration.zsh
+
+# Auto jump
+[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
+
+# Kube-alias: Print the full command before running it
+if [ $commands[kubectl] ]; then
+    function kubectl() { echo "+ kubectl $@">&2; command kubectl $@; }
+fi
+
+# asdf
+[ -f /usr/local/opt/asdf/asdf.sh ] && . /usr/local/opt/asdf/asdf.sh
+
+# pyenv
+eval "$(pyenv init -)"
+
+# Add Startify
+# zsh-startify
+
+# mozjpeg
+export PATH="/usr/local/opt/mozjpeg/bin:$PATH"
+
+# Starship theme
+eval "$(starship init zsh)"
+
+neofetch
